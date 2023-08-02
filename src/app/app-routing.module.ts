0a81c46b985b51be53ef0f781e94439eedca3e47
harinimarripudi <marripudiharni@gmail.com>
2023-08-02 20:32:25 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UploadComponent } from './upload/upload.component';
+import { DisplayComponent } from './display/display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path:"",component:UploadComponent},
+  {path:"upload",component:UploadComponent},
+  {path:"display",component:DisplayComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
